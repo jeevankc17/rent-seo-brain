5370bf52fbb0d37ffc778445f05750bd7a5f7f96
Jeevan KC <jkc5186@gmail.com>
2025-08-26 21:13:49 +0545

    blog page responsive done

--- app/google/page.tsx
+++ app/google/page.tsx
@@ -1,30 +1,26 @@
+import CMS from "@components/CMS/Hero-Section";
 import { AdOptionsSection } from "@components/Google/AdOptionsSection";
 import { AdOverviewSection } from "@components/Google/AdOverviewSection";
-import { GoogleProposition } from "@components/Google/GoogleProposition";
 import { FAQSection } from "@components/Google/FAQSection";
 
 import GoogleFeatures from "@components/Google/GoogleFeatures";
 import GoogleLP from "@components/Google/GoogleLP";
 import { GoogleTestimonial } from "@components/Google/GoogleTestimonial";
 import HeroSection from "@components/Google/HeroSection";
-import { ProcessSection } from "@components/Google/Process";
+import { ProcessSection } from "@components/Google/Process"
 import { ServicesSection } from "@components/Google/ServiceSection";
 import { StatisticsSection } from "@components/Google/StatisticsSection";
 import { StatsSection } from "@components/Google/StatsSection";
+import TestimonialsSection from "@components/Google/TestimonialsSection";
 import { ToolsIntegrationSection } from "@components/Google/ToolsIntegrationsection";
 import { ToolsSection } from "@components/Google/ToolsSection";
 import Wireframe from "@components/Landing-Page/Wireframe";
-import { GoogleTechnologyStackSection } from "@components/Google/GoogleTechnologyStackSection";
-import HeroPage from "@components/Google/HeroPage";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center bg-grey-100 gap-8 md:gap-16 lg:gap-[132px] pb-8 md:pb-16 lg:pb-[132px]">
+    <main className="flex flex-col items-center bg-white gap-8 md:gap-16 lg:gap-[132px] pb-8 md:pb-16 lg:pb-[132px]">
       <section className="relative w-full h-auto min-h-[400px] md:min-h-[600px] lg:h-[960px] bg-neutral-900">
-        <HeroSection />
-      </section>
-      <section className="relative bg-grey-100 h-auto w-full px-4 lg:h-auto lg:px-[157px]">
-        <GoogleProposition />
+        <GoogleLP />
       </section>
       <section className="relative w-full h-auto px-4 md:px-8 lg:px-[154px]">
         <AdOverviewSection />
@@ -49,9 +45,6 @@ export default function Home() {
       </section>
       <section className="relative w-full h-auto">
         <FAQSection />
-      </section>
-      <section className="relative w-full h-auto lg:h-[735px]">
-        <GoogleTechnologyStackSection />
       </section>
       <section className="relative w-full h-auto px-4 md:px-8 lg:px-[156px]">
         <ToolsIntegrationSection />
